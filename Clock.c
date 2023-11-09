4ce3e427c4d71a02027c7f8cc6dad71e5da08540
BrandonKahn25 <kahnbrandon25@gmail.com>
2023-11-09 15:55:07 -0600

    Added Clock file

---
+++ Clock.c
@@ -0,0 +1,16 @@
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+
+#include <stdio.h>
+
+int main(void) {
+
+    
+    while(1) {
+        
+    }
+    return 0 ;
+}
